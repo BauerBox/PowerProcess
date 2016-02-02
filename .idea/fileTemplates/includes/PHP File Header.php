8f2607916777ecb2d0f32195657fce0d8125718b
Don Bauer <don.bauer@dtn.com>
2016-02-02 12:18:03 -0600

    Added PhpStorm metadata files

---
+++ .idea/fileTemplates/includes/PHP File Header.php
@@ -0,0 +1,9 @@
+/**
+ * This file is a part of the PowerProcess package for PHP by BauerBox Labs
+ *
+ * @copyright
+ * Copyright (c) 2013-2016 Don Bauer <lordgnu@me.com> BauerBox Labs
+ *
+ * @license https://github.com/BauerBox/PowerProcess/blob/master/LICENSE MIT License
+ */
+ 
